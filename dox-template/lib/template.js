a8cf0a0c4fb73d1c499fa8c3f2eca1f881e8ffba
lwj <lwj19861218@163.com>
2012-10-12 14:27:28 +0800

    fix bug of copy

---
+++ dox-template/lib/template.js
@@ -0,0 +1,53 @@
+var mu = require('../../mu2/lib/mu.js'),
+    fs = require('fs'),
+    presenter = require('./presenter.js'),
+    extend = require('./utils.js').extend,
+    version = require('./version.js')
+    counter = 0
+
+mu.root = __dirname + '/templates'
+
+var defaults = {
+  release: '?.?.?',
+  name: '?',
+  styles: [__dirname, 'stylesheets', 'light.css'].join('/')
+}
+
+function outUndefined (elem) {
+  return !!elem
+}
+
+var Template = function (options) {
+  this.settings = extend(defaults, options || {})
+}
+
+Template.version = version
+
+Template.prototype.styles = function () {
+  return fs.readFileSync(this.settings.styles)
+}
+
+Template.prototype.render = function (raw) {
+  var docs = raw.map(presenter).filter(outUndefined)
+
+  var modules = docs.filter(function (doc) {
+    return doc.module
+  })
+
+  modules.forEach(function (module) {
+    module.methods = docs.filter(function (doc) {
+      return doc.parent === module.name
+    })
+  })
+
+  return mu.compileAndRender('template.mustache', {
+    modules: modules,
+    raw: JSON.stringify(modules),
+    version: this.settings.release,
+    library_name: this.settings.name,
+    styles: this.styles()
+  })
+
+}
+
+module.exports = Template

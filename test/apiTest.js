6a5418f23844e0e3e9b3f2c35285dc9819658fac
lwj <lwj19861218@163.com>
2012-11-08 14:46:52 +0800

    fix bug

--- test/apiTest.js
+++ test/apiTest.js
@@ -3,5 +3,5 @@ var Doxmake = require('../lib/doxmake');
 new Doxmake({
 	name: "apiTest",
 	release: "0.1.2",
-	directory: "/home/lwj/workspace/pomelo-core/lib"
+	directory: "/home/lwj/jsLibrary/"
 }).run();
